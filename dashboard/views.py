f67d38b78e6125481a14517d7713b73e044b9fa4
Tinashe Chihoro <tinashechihoro@gmail.com>
2023-01-16 12:48:38 +0200

    added the accounts models

---
+++ dashboard/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+
+class DashboardView(TemplateView):
+    template_name = 'dashboard/base.html'
+
+
--- person_details/models.py
+++ person_details/models.py
@@ -9,14 +9,14 @@ class NextOfKeen(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     middle_name = models.CharField(max_length=255, null=True, blank=True)
-    national_identity_number = models.CharField(max_length=50)
+
     gender = models.CharField(max_length=10, choices=GENDER_CHOICES)
-    date_of_birth = models.DateField()
+
     address = models.TextField()
     home_number = models.CharField(max_length=255, null=True, blank=True)
     mobile = models.CharField(max_length=255, null=True, blank=True)
     email = models.EmailField(null=True, blank=True)
-    relationship = models.CharField(max_length=255)
+    relationship = models.CharField(max_length=255, null=True, blank=True)
 
 
 
